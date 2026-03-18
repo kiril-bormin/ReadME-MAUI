5ce362ac40d32cecaf119a7d3f8d47facf4111bd
Kiril Bormin <borminnkiril@gmail.com>
2026-03-18 16:04:22 +0100

    Initial Commit

---
+++ ReadME-MAUI/ReadME-MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReadME_MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ReadME-MAUI/ReadME-MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadME_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
