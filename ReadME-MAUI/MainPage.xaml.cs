b53dbffaaaca1b65c33b61ec21279e72723f9cf1
Kiril Bormin <borminnkiril@gmail.com>
2026-04-01 16:29:55 +0200

    add class Book [WIP]

--- ReadME-MAUI/MainPage.xaml.cs
+++ ReadME-MAUI/MainPage.xaml.cs
@@ -1,23 +1,35 @@
 ﻿using VersOne.Epub;
+using ReadME_MAUI.Models;
+using System.Collections.ObjectModel;
 namespace ReadME_MAUI
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage() => InitializeComponent();
+        public ObservableCollection<Book> MyBooks { get; set; } = new(); //crée la db en ram
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
         private async void ImportBook(object sender, EventArgs e)
         {
             try
             {
                 var result = await FilePicker.Default.PickAsync(); // demander à l'os d'ouvrir l'explorateur
                 if (result == null) return; 
-                EpubBook book = await EpubReader.ReadBookAsync(result.FullPath); //versone extrait les metadonnées
+                EpubBook book = await EpubReader.ReadBookAsync(result.FullPath); //VersOne extrait les metadonnées
 
-                await DisplayAlert("Livre ", $"Titre : {book.Title}\nAuteur : {book.Author} \n{book.CoverImage}", "Cool");
+                // titre, description, boutons
+                await DisplayAlert("Livre ", $"Titre : {book.Title}\nAuteur : {book.Author} \n{book.CoverImage}", "Cool"); 
             }
-            catch (Exception ex)
+            catch (Exception error)
             {
-                await DisplayAlert("Erreur d'importation de livre", ex.Message, "OK");
+                await DisplayAlert("Erreur d'importation de livre", error.Message, "D'accord");
             }
         }
+
+
     }
 }
---
+++ ReadME-MAUI/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadME_MAUI.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string File { get; set; }
+        public ImageSource Image { get; set; }
+
+    }
+}
